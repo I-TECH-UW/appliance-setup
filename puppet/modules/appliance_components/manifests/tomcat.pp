057a56c91d02b130a166f5244f653ccb1172a7ec
Ben Smith <bens23@uw.edu>
2017-08-14 14:19:22 -0700

    update tomcat installer

--- puppet/modules/appliance_components/manifests/tomcat.pp
+++ puppet/modules/appliance_components/manifests/tomcat.pp
@@ -5,4 +5,14 @@
 class appliance_components::tomcat {
   include appliance_components::java
   include ::tomcat
+  
+  package { 'tomcat8':
+    ensure => installed,
+  }
+	  
+  service { 'tomcat8':
+	ensure    => running,
+	enable    => true,
+	subscribe => Package['tomcat8'],
+  }
 }
